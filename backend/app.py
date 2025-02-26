55fdc06d3cd46e4c5c262dbdbeccffa45c78eef9
pavan <pavanbejawada4376@gmail.com>
2025-02-26 14:45:36 +0530

    cors issue after frontend change

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ import os
 from io import BytesIO
 
 app = Flask(__name__)
-CORS(app, origins=["https://invisihide-frontend.onrender.com","http://localhost:3000"],supports_credentials=True)
+CORS(app, origins=["https://invisihide-frontend.onrender.com"],supports_credentials=True)
 
 def text_to_binary(text):
     return ''.join(format(ord(c), '08b') for c in text)
